87d7e746fe041c575a42adb59e27302180277396
ucho456job <ucho456job@gmail.com>
2024-10-22 19:32:39 +0900

    save

--- internal/application/mock/mock.go
+++ internal/application/mock/mock.go
@@ -3,4 +3,4 @@
 package mock
 
 // The command to create a mock of a usecase file is as follows:
-// mockgen -source=internal/application/{domain}/{fileName}.go -destination=internal/application/mock/mock_{fileName}.go -package=mock
+// mockgen -source=internal/application/authentication/signup_usecase.go -destination=internal/application/mock/mock_signup_usecase.go -package=mock
