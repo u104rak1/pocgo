592d733fc82fd2f1e22373760c0262294d88c386
u104rak1 <watapqxyz@gmail.com>
2024-12-23 10:43:58 +0900

    save

--- internal/infrastructure/postgres/repository/repository_test.go
+++ internal/infrastructure/postgres/repository/repository_test.go
@@ -6,9 +6,44 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/u104rak1/pocgo/internal/config"
+	"github.com/u104rak1/pocgo/internal/infrastructure/postgres/repository"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 )
+
+type testStruct struct {
+	ID   int
+	Name string
+}
+
+func TestExecDB(t *testing.T) {
+	repo, mock, ctx, bunDB := PrepareTestRepository(t, func(db *bun.DB) *repository.Repository[testStruct] {
+		return repository.NewRepository[testStruct](db)
+	})
+
+	t.Run("トランザクションなしの場合", func(t *testing.T) {
+		execDB := repo.ExecDB(ctx)
+		assert.Equal(t, bunDB, execDB)
+	})
+
+	t.Run("トランザクションありの場合", func(t *testing.T) {
+		mock.ExpectBegin()
+
+		tx, err := bunDB.BeginTx(ctx, nil)
+		assert.NoError(t, err)
+
+		mock.ExpectRollback()
+		defer tx.Rollback()
+
+		txCtx := context.WithValue(ctx, config.CtxTransactionKey(), tx)
+		execDB := repo.ExecDB(txCtx)
+		assert.Equal(t, tx, execDB)
+	})
+
+	err := mock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
 
 // テスト用のリポジトリを作成するためのヘルパー関数です。
 func PrepareTestRepository[T any](t *testing.T, newRepo func(db *bun.DB) T) (T, sqlmock.Sqlmock, context.Context, *bun.DB) {
